4dc72a16663df8a6840b6288fd612e208bf6a4ab
amg-k <adamgorz@gmail.com>
2023-07-08 14:29:33 +0200

    Merge PrimitiveTypeObjectTester to one file

--- PrimitiveTypeObjectTester.java
+++ PrimitiveTypeObjectTester.java
@@ -6,3 +6,23 @@ public class PrimitiveTypeObjectTester {
         p.doIt();
         }
     }
+
+class PrimitiveTypeObjects {
+    private Integer iObj;
+    private int i;
+
+    public void doIt() {
+        this.iObj = 4;
+        this.i = this.iObj;
+        System.out.println(this.i);
+        System.out.println(this.iObj);
+    }
+
+    public int getI() {
+        return i;
+    }
+
+    public Integer getIobj() {
+        return iObj;
+    }    
+}    
