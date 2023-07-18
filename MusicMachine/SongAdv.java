c81471b503603274e2cb599cab0d02a4ae42ff9d
amg-k <adamgorz@gmail.com>
2023-07-18 12:39:24 +0200

    Add SongAdv class to package MusicMachine

---
+++ MusicMachine/SongAdv.java
@@ -0,0 +1,41 @@
+package MusicMachine;
+
+public class SongAdv {
+    private final String title;
+    private final String artist;
+    private final String genre;
+    private final int year;
+    private final int playsNum;
+
+    public SongAdv(String title, String artis, String genre, int year, int playsNum) {
+        this.title = title;
+        this.artist = artis;
+        this.genre = genre;
+        this.year = year;
+        this.playsNum = playsNum;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getArtis() {
+        return artist;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public int getYear() {
+        return year;
+    }
+    
+    public int getPlaysNum() {
+        return playsNum;
+    }
+
+    public String toString() {
+        return title;
+    }
+}
