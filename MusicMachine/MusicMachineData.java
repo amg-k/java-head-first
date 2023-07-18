4830e73dc0b5a3874d7c0a0f0932cdb230767481
amg-k <adamgorz@gmail.com>
2023-07-18 13:15:24 +0200

    Add test data class MusicMachineData

---
+++ MusicMachine/MusicMachineData.java
@@ -0,0 +1,33 @@
+package MusicMachine;
+
+import java.util.*;
+
+public class MusicMachineData {
+    public static class SongsAdv {
+        public static List<SongAdv> getSongsAdv() {
+            return List.of(
+                new SongAdv("$10", "Hitchhiker", "Electronic", 2016, 183),
+                new SongAdv("Havana", "Camila Cabello", "R&B", 2017, 324),
+                new SongAdv("Cassidy", "Greatful Dead", "Rock", 1972, 123),
+                new SongAdv("50 ways", "Paul Simon", "Soft rock", 1975, 199),
+                new SongAdv("Hurt", "Nine Inch Nails", "Industrial rock", 1995, 257),
+                new SongAdv("Silence", "Delerim", "Electronic", 1999, 134),
+                new SongAdv("Hurt", "Johny Cash", "Soft rock", 2002, 392),
+                new SongAdv("Watercolour", "Pendulum", "Electronic", 2012, 155),
+                new SongAdv("Mniej niż zero", "Lady Punk", "Rock", 1983, 259),
+                new SongAdv("With a Little Help from My Friends", "The Beatles", "Rock", 1967, 168),
+                new SongAdv("Come Together", "The Beatles", "Blues rock", 1968, 173),
+                new SongAdv("Come Together", "Ike & Tina Turner", "Rock", 1970, 165),
+                new SongAdv("With a Little Help from My Friends", "Joe Cocker", "Rock", 1968, 46),
+                new SongAdv("Immigrant Song", "Karen O", "Industrial rock", 2011, 12),
+                new SongAdv("Breathe", "The Prodigy", "Electronic", 1996, 337),
+                new SongAdv("What's Going On", "Gaye", "R&B", 1971, 420),
+                new SongAdv("Autobiografia", "Perfect", "Rock", 1982, 480),
+                new SongAdv("All My Love", "Led Zeppelin", "Rock", 1979, 402),
+                new SongAdv("I'm not a woman, I'm a god", "Halsey", "Alternative rock", 2021, 384),
+                new SongAdv("Pasos de cero", "Pablo Alboran", "Latino", 2014, 117),
+                new SongAdv("Smooth", "Santana", "Latino", 1999, 244),
+                new SongAdv("Immigrant Song", "Led Zeppelin", "Rock", 1970, 484));
+        }
+    }
+}
