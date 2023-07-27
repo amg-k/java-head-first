76767973ce3a74f2d413b074177a18362a1074fa
amg-k <adamgorz@gmail.com>
2023-07-27 16:57:51 +0200

    GUI, nested class implements ActionListener

--- GUI/TesterGUI.java
+++ GUI/TesterGUI.java
@@ -2,13 +2,14 @@ package GUI;
 
 import java.awt.*;
 import java.awt.event.*;
-
 import javax.swing.*;
 
-public class TesterGUI implements ActionListener {
+public class TesterGUI {
     private JFrame myFrame;
     private MyGraphicPanel myPanel;
-    private JButton myButton;
+    private JButton myButtonRect;
+    private JButton myButtonLabel;
+    private JLabel myLabel;
 
     public static void main(String[] args) {
         TesterGUI myTester = new TesterGUI();
@@ -17,19 +18,33 @@ public class TesterGUI implements ActionListener {
 
     public void doIt() {
         myFrame = new JFrame();
+        myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         myPanel = new MyGraphicPanel();
-        myButton = new JButton("New Color!");
+        myButtonRect = new JButton("New Color!");
+        myButtonLabel = new JButton("Change label!");
+        myLabel = new JLabel("Lorem ipsum...");
 
-        myButton.addActionListener(this);
+        myButtonRect.addActionListener(new RectListener());
+        myButtonLabel.addActionListener(new LabelListener());
 
         myFrame.getContentPane().add(BorderLayout.CENTER, myPanel);
-        myFrame.getContentPane().add(BorderLayout.SOUTH, myButton);
-        myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        myFrame.getContentPane().add(BorderLayout.SOUTH, myButtonRect);
+        myFrame.getContentPane().add(BorderLayout.WEST, myLabel);
+        myFrame.getContentPane().add(BorderLayout.EAST, myButtonLabel);
+        
         myFrame.setSize(600, 500);
         myFrame.setVisible(true);
     }
 
-    public void actionPerformed(ActionEvent myEvent) {
-        myFrame.repaint();
+    class RectListener implements ActionListener {
+        public void actionPerformed(ActionEvent myEvent) {
+            myFrame.repaint();
+        }    
+    }
+
+    class LabelListener implements ActionListener {
+        public void actionPerformed(ActionEvent myEvent) {
+            myLabel.setText("...dolor sit amet...");
+        }    
     }
 }
