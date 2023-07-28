7edff1ebd0ba49e08bc5676286d90d968237b12c
amg-k <adamgorz@gmail.com>
2023-07-28 19:07:40 +0200

    Add flag event using ControllerEventListener

--- SoundBox/MusicVisualizer.java
+++ SoundBox/MusicVisualizer.java
@@ -5,15 +5,25 @@ import static javax.sound.midi.ShortMessage.*;
 
 public class MusicVisualizer {
     public static void main(String[] args) {
+        MusicVisualizer myVisualizer = new MusicVisualizer();
+        myVisualizer.playSounds();
+    }
+
+    public void playSounds() {
         try {
             Sequencer mySequencer = MidiSystem.getSequencer();
             mySequencer.open();
+
+            int[] eventFlagNum = {127};
+            mySequencer.addControllerEventListener(myEvent -> System.out.println("note played"), 
+                                                    eventFlagNum);
 
             Sequence mySequence = new Sequence(Sequence.PPQ, 4);
             Track myTrack = mySequence.createTrack();
 
             for (int i = 50; i < 103; i += 4) {
                 myTrack.add(createEvent(NOTE_ON, 1, i, 100, i - 49));
+                myTrack.add(createEvent(CONTROL_CHANGE, 1, 127, 0, i - 49));
                 myTrack.add(createEvent(NOTE_OFF, 1, i, 100, i - 47));
             }
             
