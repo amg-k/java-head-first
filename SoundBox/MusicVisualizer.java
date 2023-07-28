94d7c2dc77287a284cb7ed950b018439a5937eff
amg-k <adamgorz@gmail.com>
2023-07-28 20:06:49 +0200

    Add visualizer, random notes

--- SoundBox/MusicVisualizer.java
+++ SoundBox/MusicVisualizer.java
@@ -1,9 +1,18 @@
 package SoundBox;
 
 import javax.sound.midi.*;
+import javax.swing.*;
+import java.awt.*;
+
 import static javax.sound.midi.ShortMessage.*;
 
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
 public class MusicVisualizer {
+    JFrame myFrame;
+    GraphicPanel myPanel;
+
     public static void main(String[] args) {
         MusicVisualizer myVisualizer = new MusicVisualizer();
         myVisualizer.playSounds();
@@ -14,17 +23,19 @@ public class MusicVisualizer {
             Sequencer mySequencer = MidiSystem.getSequencer();
             mySequencer.open();
 
+            createPanelFrame();
+
             int[] eventFlagNum = {127};
-            mySequencer.addControllerEventListener(myEvent -> System.out.println("note played"), 
-                                                    eventFlagNum);
+            mySequencer.addControllerEventListener(myPanel, eventFlagNum);
 
             Sequence mySequence = new Sequence(Sequence.PPQ, 4);
             Track myTrack = mySequence.createTrack();
 
-            for (int i = 50; i < 103; i += 4) {
-                myTrack.add(createEvent(NOTE_ON, 1, i, 100, i - 49));
-                myTrack.add(createEvent(CONTROL_CHANGE, 1, 127, 0, i - 49));
-                myTrack.add(createEvent(NOTE_OFF, 1, i, 100, i - 47));
+            for (int i = 1; i < 64; i += 4) {
+                int note = getRandomInt(21, 109);
+                myTrack.add(createEvent(NOTE_ON, 1, note, 100, i));
+                myTrack.add(createEvent(CONTROL_CHANGE, 1, 127, 0, i));
+                myTrack.add(createEvent(NOTE_OFF, 1, note, 100, i + 2));
             }
             
             mySequencer.setSequence(mySequence);
@@ -33,6 +44,11 @@ public class MusicVisualizer {
         } catch (Exception ex) {
             ex.printStackTrace();
         }
+    }
+
+    int getRandomInt(int min, int max) {
+            int myRandomInt = ThreadLocalRandom.current().nextInt(min, max);
+            return myRandomInt;
     }
 
     public static MidiEvent createEvent(int command, int channel, int data1, int data2, int tick) {
@@ -45,5 +61,48 @@ public class MusicVisualizer {
             ex.printStackTrace();
         }
         return myEvent;
+    }
+
+    public void createPanelFrame() {
+        myPanel = new GraphicPanel();
+        myFrame = new JFrame();
+        myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        myFrame.getContentPane().add(BorderLayout.CENTER, myPanel);
+        myFrame.setSize(600, 500);
+        myFrame.setVisible(true);
+    }
+
+    class GraphicPanel extends JPanel implements ControllerEventListener {
+        private boolean myFlag = false;
+
+        public void controlChange(ShortMessage myEvent) {
+            myFlag = true;
+            repaint();
+        }
+
+        public void paintComponent(Graphics g) {
+            if (myFlag) {
+                g.setColor(getRandomColor());
+
+                int myHeight = getRandomInt(10, 120);
+                int myWidth = getRandomInt(10, 120);
+
+                int xValue = getRandomInt(10, 200);
+                int yValue = getRandomInt(10, 200);
+
+                g.fillRect(xValue, yValue, myWidth, myHeight);
+                myFlag = false;
+            }
+        }
+
+        private Color getRandomColor() {
+            Random myRandom = new Random();
+            int myRed = myRandom.nextInt(256);
+            int myGreen = myRandom.nextInt(256);
+            int myBlue = myRandom.nextInt(256);
+    
+            Color myRandomColor = new Color(myRed, myGreen, myBlue);
+            return myRandomColor;  
+        }
     }
 }
